1a60f0d3fd9670c0f58e21aef61ff0e36de03331
Rinat <smirnov-rinat90@yandex.ru>
2023-09-09 18:24:22 +0400

    Floud: add file  solution example 10, 13, 15

---
+++ Example13/exercise_13.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+namespace Example13  {
+class exercise_13 { 
+
+static void Main() { 
+
+Console.Clear();
+
+System.Console.WriteLine("Введи число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(Number);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("Третья цифра : " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет!");
+}
+}}}
---
+++ Example15/exercise_15.cs
@@ -0,0 +1,28 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+namespace Example13  {
+class exercise_13 { 
+
+static void Main() { 
+
+Console.Clear();
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void DayWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Этот день недели - выходной.");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Нет такого дня в неделе.");
+  }
+  else Console.WriteLine("Этот день рабочий, не выходной.");
+}
+
+DayWeek(dayNumber);
+
+}}}
